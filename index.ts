ce62a8e5b2255e180b9b256c3dca4efef7d49d41
Luke Askins <lukeaskins@gmail.com>
2021-02-12 11:04:13 -0600

    Add gitignore

--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ const app = express();
 const PORT = 8000;
 
 app.get("/", (req, res) => res.send("Express Typescript Server"));
+app.post("/", (req, res) => res.send("Got a POST request"));
+app.put("/", (req, res) => res.send("Got a PUT request"));
+app.delete("/", (req, res) => res.send("Got a DELETE request"));
 app.listen(PORT, () => {
   console.log(`[server]: Server is running on https://localhost:${PORT} `);
 });
